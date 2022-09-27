ba7e6fb45bf7a102c04d4c415a705bfdf9787c5c
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-26 21:28:44 -0400

    Feat: roadtrip call rejected if no api key provided

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::RoadtripController < ApplicationController
 
   def create
-    #need api key check still
-    roadtrip = RoadtripFacade.road_trip(params[:roadtrip_params][:origin],params[:roadtrip_params][:destination])
+    if params[:roadtrip_params][:api_key].empty?
+      render :json => {:error => "API key missing"}, :status => 401
+    else
+      roadtrip = RoadtripFacade.road_trip(params[:roadtrip_params][:origin],params[:roadtrip_params][:destination])
 
-    render json: RoadtripSerializer.new(roadtrip).response, status: 200
+      render json: RoadtripSerializer.new(roadtrip).response, status: 200
+    end
   end
 
 end
